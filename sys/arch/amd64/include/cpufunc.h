6489cc9466420fb7919908393133e83fb06309a6
Hans-Joerg Hoexer <hshoexer@genua.de>
2025-02-13 10:51:30 +0100

    kern/vmm: add vmterminate() function; update MSR_PROTO_TERMINATION_REQ

--- sys/arch/amd64/include/cpufunc.h
+++ sys/arch/amd64/include/cpufunc.h
@@ -447,6 +447,20 @@ vmgexit(void)
 	__asm volatile("rep; vmmcall");
 }
 
+/* Request VM termination from hypervisor. */
+static __inline void
+vmterminate(void)
+{
+	__asm volatile(
+		"	movl	$MSR_PROTO_TERMINATION_REQ, %%rdx	;"
+		"	movl	$MSR_SEV_GHCB, %%rcx			;"
+		"	wrmsr						;"
+		"	rep	vmmcall					;"
+		"1:	hlt						;"
+		"	jmp	1b					;"
+	    : :);
+}
+
 void amd64_errata(struct cpu_info *);
 void cpu_ucode_setup(void);
 void cpu_ucode_apply(struct cpu_info *);
