50ed2dd07b6ffebf7cc7602ed8a0e23977a1a59e
Hans-Joerg Hoexer <hshoexer@genua.de>
2025-02-26 18:04:03 +0100

    kern: log SEV-SNP guestmode

--- sys/arch/amd64/amd64/identcpu.c
+++ sys/arch/amd64/amd64/identcpu.c
@@ -712,10 +712,13 @@ identifycpu(struct cpu_info *ci)
 		    CPUID_AMDSEV_EDX_BITS);
 		amd64_pos_cbit = (ci->ci_feature_amdsev_ebx & 0x3f);
 		amd64_min_noes_asid = ci->ci_feature_amdsev_edx;
-		if (cpu_sev_guestmode && CPU_IS_PRIMARY(ci))
+		if (cpu_sev_guestmode && CPU_IS_PRIMARY(ci)) {
 			printf("\n%s: SEV%s guest mode", ci->ci_dev->dv_xname,
-			    ISSET(cpu_sev_guestmode, SEV_STAT_ES_ENABLED) ?
-			    "-ES" : "");
+			    ISSET(cpu_sev_guestmode, SEV_STAT_SNP_ACTIVE) ?
+			    "-SNP" :
+			    (ISSET(cpu_sev_guestmode, SEV_STAT_ES_ENABLED) ?
+			    "-ES" : ""));
+		}
 	}
 
 	printf("\n");
